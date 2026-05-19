f25545dd473e62d034938142609bb8e7fab7f381
Santiago4566 <carrizosantiago64@gmail.com>
2026-05-19 13:42:55 -0900

    primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+
+int b;
+
+a = 10;
+
+b = a;
+
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+
+//read devuelve el codigo ascii de la tecla presionada
+
+
