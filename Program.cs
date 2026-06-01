aa6cb4ed4bdc48a061f7bb25e320c37ef2bcd354
RudeVal <vrudellifacu@gmail.com>
2026-06-01 17:52:09 -0300

    ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+Console.WriteLine("Hello, World!");
 
 int a;
 
@@ -10,3 +11,27 @@ b = a;
 
 Console.WriteLine("valor de a:" + a);
 Console.WriteLine("valor de b:" + b);
+*/
+
+//-----------Ejercicio 1----------------
+
+Console.WriteLine("Ingrese un numero: ");
+    string texto = Console.ReadLine();
+
+bool valido = int.TryParse(texto, out int numero); //verificamos que sea un num valido
+if(!valido) Console.WriteLine("No ingresó un número");
+else if(numero <= 0) Console.WriteLine("El número debe ser mayor que 0"); //verifica num>0
+else {
+    Console.WriteLine("Numero valido");
+    int aux = numero;
+    int invertido = 0;
+
+    while(aux>0)
+    {
+        invertido = (invertido * 10) + (aux % 10);
+        aux = aux / 10; 
+    }
+
+    Console.WriteLine($"Numero invertido: {invertido}");
+}
+
