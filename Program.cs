94bdd16bea806e101e8952433b36d9e48020c6f9
RudeVal <vrudellifacu@gmail.com>
2026-06-04 17:47:30 -0300

    calculadoraV1

--- Program.cs
+++ Program.cs
@@ -37,6 +37,112 @@ else {
 }
 */
 
+//----------Ejercicio 2--------
+using System.Diagnostics.Contracts;
+using System.Net;
+
+Console.WriteLine("CALCULADORA");
+Console.WriteLine(
+    "-1 Sumar\n" +
+    "-2 Restar\n" +
+    "-3 Multiplicar\n" +
+    "-4 Dividir\n" +
+    "-0 Salir"
+);
+
+
+int continuar = 0;
+do
+{
+
+    Console.WriteLine("Ingrese el primer numero: ");
+    int a = PedirNumero();
+
+    Console.WriteLine("Ingrese el segundo numero: ");
+    int b = PedirNumero();
+
+    Console.WriteLine("Ingrese el numero de operacion: ");
+    int eleccion = PedirNumero();
+
+    int resultado = 0;
+    switch (eleccion)
+    {
+        case 1: 
+            resultado = sumar(a, b);
+            break;
+        case 2: 
+            resultado = restar(a, b);
+            break;
+        case 3: 
+            resultado = multiplicar(a, b);
+            break;
+        case 4: 
+            if (b != 0) resultado = dividir(a, b);
+            break;
+        default: 
+            Console.WriteLine("Opción no válida. Intente de nuevo.");
+            break;
+    }
+
+    if (eleccion == 4 && b== 0)
+    {
+        Console.WriteLine("Error");
+    }
+    else
+    {
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+
+    Console.WriteLine("...Continuar? 0-no, 1-si");
+        continuar = PedirNumero();
+    
+} while (continuar == 1);
+
+static int PedirNumero()
+{
+    int numero;
+    bool valido;
+
+    do
+    {
+        string entrada = Console.ReadLine();
+
+        valido = int.TryParse(entrada, out numero);
+
+        if (!valido)
+        {
+            Console.WriteLine("Numero no valido, ingrese otro");
+        }
+
+    } while (!valido);
+
+    return numero;
+}
+
+static int sumar(int n1, int n2)
+{
+    int resultado = n1+n2;
+    return resultado;
+}
+
+static int restar(int n1, int n2)
+{
+    int resultado = n1-n2;
+    return resultado;
+}
+
+static int multiplicar(int n1, int n2)
+{
+    int resultado = n1*n2;
+    return resultado;
+}
+
+static int dividir(int n1, int n2)
+{
+    int resultado = n1/n2;
+    return resultado;
+}
+
 
 
 
